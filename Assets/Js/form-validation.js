595cdd0f22fd9598151e9540feebdb7a00d6927e
WangleyQuinhones <wangleyquinhones@gmail.com>
2023-07-23 23:54:57 -0400

    Update form-validation.js

--- Assets/Js/form-validation.js
+++ Assets/Js/form-validation.js
@@ -1,111 +1,131 @@
-'use scritic';
-// let ou const ao invés de var
-// IIFE - immediately Invoked Function Expression 
+'use strict';
 
 (() => {
     const form = document.querySelector('[data-form]')
+    const progressBar = document.querySelector('[data-requirement-progressbar]')
     const fields = {}
     const requirements = {}
+    const state = { passwordStrength: 0 }
+    
+    const styleProgressBar = () => {
+        progressBar.style.width = `${state.passwordStrength}%`
+        progressBar.dataset.percentage = state.passwordStrength
+    }
 
-    const showMessageError = (filde, message) => {
-        const { Element, errorElement,} = filde
-        Element.classList.add('error')
+    const showMessageError = (field, message) => {
+        const { element, errorElement } = field
+        element.classList.add('error')
         errorElement.style.display = 'block'
-        errorElement.textContent = 'Este campo é obrigatório!'
-
+        errorElement.textContent = message
     }
 
     const hideMessageError = (field) => {
-        const {Element, errorElement, isRequired} = field
-        Element.classList.remove('error')
-                errorElement.style.display = 'none'
-                errorElement.textContent = ''
-    } 
+        const { element, errorElement } = field
+        element.classList.remove('error')
+        errorElement.style.display = 'none'
+        errorElement.textContent = ''
+    }
+
+    const checkPasswordStregth = (requirementName, condition) => {
+        if(condition) {
+            state.passwordStrength += 25
+            requirements[requirementName].classList.add('checked')
+        } else {
+            requirements[requirementName].classList.remove('checked')
+        }
+    }
 
     const validateRequiredFields = () => {
-        let isValid = false
-        for (const fildKey in fields){
-            const field = fields[fildKey]
-            const { Element, errorElement, isRequired } = field
-            if((!Element.value || (fildKey === 'terms' && !Element.checked && isRequired)) && isRequired ) {
-                isValid = true
-                showMessageError (field, 'Este campo é obrigatório!')
-                
-                
+        let isInvalid = false
+        for (const fieldKey in fields) {
+            const field = fields[fieldKey]
+            const { element, isRequired } = field
+            if((!element.value || (fieldKey === 'terms' && !element.checked)) && isRequired) {
+                isInvalid = true
+                showMessageError(field, 'Este campo é obrigatório!')
             }
         }
-        return isValid        
-
+        return isInvalid
     }
 
-    const onPasswordKeyup = (event) => {
-        const {value} = event.target
-        const lowerCasePattern = new RegExp(/[a-z]/)
-        const upperCasePattern = new RegExp(/[A-Z]/)
-        const numberPattern = new RegExp(/[0-9]/)
-        if(value.match(lowerCasePattern) && value.match(upperCasePattern)){
-            requirements('lowerCasePattern').classList.add('checked')
+    const validatePasswordStrength = () => {
+        let isInvalid = false
+        const field = fields['password']
+        if(state.passwordStrength < 100) {
+            isInvalid = true
+            showMessageError(field, 'Digte uma senha válida!')
         }
-       
-    }
-    
-        
-
-
-        // Regular Expression 
-        //RegExp
-
-       // Função que valida os campos do tipo checkbox e radiobutton
-
-    const onInputFocus = (event) => {       
-        hideMessageError(fields[event.target.name])                
-
+        return isInvalid
     }
 
-   const onFormsubimit = (event) => {
-    event.preventDefault()
-    if(validateRequiredFields()) return
+    const validateEmail = () => {
+        let isInvalid = false
+        const field = fields['email']
+        const { value } = field.element
+        if(!value.match(/^[\w\.]+@\w+(\.\w+)+$/)) {
+            isInvalid = true
+            showMessageError(field, 'Digte um e-mail válido!')
+        }
+        return isInvalid
+    }
 
-    alert('Dados prontos para serem enviados!')
-   }
+    const onInputPasswordKeyup = (event) => {
+        const { value } = event.target
+        state.passwordStrength = 0
+        checkPasswordStregth('lowerUpperCase', value.match(/[a-z]/) && value.match(/[A-Z]/))
+        checkPasswordStregth('number', value.match(/[0-9]/))
+        checkPasswordStregth('specialCharacter', value.match(/[!@#$%\^&*~)\[\]{}?\.(+=\._-]/))
+        checkPasswordStregth('minCharacter', value.length >= 8)
+        styleProgressBar()
+    }
+
+    const onInputFocus = (event) => {
+        hideMessageError(fields[event.target.name])
+    }
+
+    const onFormSubmit = (event) => {
+        event.preventDefault()
+        if(validateRequiredFields()) return
+        if(validateEmail()) return
+        if(validatePasswordStrength()) return
+        alert('Dados prontos para serem enviados!')
+    }
 
     const setListeners = () => {
-        form.addEventListener('submit',onFormsubimit )
-        for (const fildKey in fields) {
-            const {Element} = fields[fildKey]
-        Element.addEventListener('focus', onInputFocus)  
-        if(fildKey === 'password') Element.addEventListener('Keyup',onPasswordKeyup )      
-    
-    }
-    }
-    const setRequirementItensElements = () => {
-        const requirementItensElements = document.querySelectorAll('[data-requirement-item]')
-        for (const requirementItem of requirementItensElements) {
-        const requirementName = requirementItem.dataset ['requirementItem']
-        requirements[requirementName] = requirementItem        }
-        
+        form.addEventListener('submit', onFormSubmit)
+        for (const fieldKey in fields) {
+            const { element } = fields[fieldKey]
+            element.addEventListener('focus', onInputFocus)
+            if(fieldKey === 'password') element.addEventListener('keyup', onInputPasswordKeyup)
+        }
     }
 
+    const setRequirementItemsElements = () => {
+        const requirementItemsElements = document.querySelectorAll('[data-requirement-item]')
+        for (const requirementItem of requirementItemsElements) {
+            const requirementName = requirementItem.dataset['requirementItem']
+            requirements[requirementName] = requirementItem
+        }
+    }
 
     const setFieldElements = () => {
-        const inputElements = document.querySelectorAll('[data-input]') 
+        const inputElements = document.querySelectorAll('[data-input]')
         for (const input of inputElements) {
             const inputName = input.getAttribute('name')
             fields[inputName] = {
-                Element: input,
+                element: input,
                 errorElement: input.parentElement.querySelector('[data-error-message]'),
-                isRequired: input.hasAttribute('required') 
+                isRequired: input.hasAttribute('required')
             }
             input.removeAttribute('required')
         }
-        
     }
 
     const init = () => {
         setFieldElements()
-        setRequirementItensElements()
-         setListeners()
+        setRequirementItemsElements()
+        setListeners()
     }
-    
+
     init()
 })()
